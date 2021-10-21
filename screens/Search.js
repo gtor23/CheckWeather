d70908b7ad5f17a866e8e0104896edd3c2361660
Gerardo Torres <68238871+gtor23@users.noreply.github.com>
2021-10-21 02:58:53 -0500

    installed axios

--- screens/Search.js
+++ screens/Search.js
@@ -1,16 +1,38 @@
-import * as React from 'react';
-import { Appbar, Title } from 'react-native-paper'; //importing react-native-paper components
+import React, {useState} from 'react';
+import axios from 'axios'
+import { TextInput, Button} from 'react-native-paper'; //importing react-native-paper components
 import {View, Text} from 'react-native' //importing react native components
 import {Header} from './index'
 
 const Search = () => {
+    const [city, setCity] = useState('');
+    const [cities, setCities] = useState([]);
 
+    const fetchCities = (text) => {
+        setCity(text);
+    }
 
   return (
     <View style = {{flex:1}}>
         <Header />
-        <Text>Search Screen</Text>
+        <TextInput 
+        label='City Name' 
+        theme= {{colors:{primary:'#00aaff'}}}
+        value = {city}
+        onChangeText = {(text) => fetchCities(text)}
+        />
+        
+        <Button
+        icon = 'content-save'
+        mode = 'contained'
+        theme = {{colors: {primary:'#00aaff'}}}
+        style = {{margin:20}}
+        onPress = {() => console.log('Pressed')}>
+            <Text style = {{color: 'white'}}> Press Me </Text>
+        </Button>
     </View>
+
+
   );
 };
 
